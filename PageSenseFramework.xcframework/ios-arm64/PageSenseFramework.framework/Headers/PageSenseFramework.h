e01a70d18d299f1f9ee0c4ee7146b7c543845b3e
Thiagesh <thiageshsenguttuvan.s@zohocorp.com>
2026-04-09 11:59:57 +0530

    Revert "PageSenseFramework.xcframework.zip"
    This reverts commit 977fad8b42b4b18b1ecbd3bf993e45cb3a7551e8.

---
+++ PageSenseFramework.xcframework/ios-arm64/PageSenseFramework.framework/Headers/PageSenseFramework.h
@@ -0,0 +1,20 @@
+//
+//  PageSenseFramework.h
+//  PageSenseFramework
+//
+//  Created by thiagesh s. senguttuvan on 05/02/20.
+//  Copyright © 2020 thiagesh s. senguttuvan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Reachability.h"
+
+//! Project version number for PageSenseFramework.
+FOUNDATION_EXPORT double PageSenseFrameworkVersionNumber;
+
+//! Project version string for PageSenseFramework.
+FOUNDATION_EXPORT const unsigned char PageSenseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PageSenseFramework/PublicHeader.h>
+
+
